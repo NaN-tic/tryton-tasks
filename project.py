cdd854d6a8fb5e4edd076bcebb9834c90f023c18
Sim? Albert i Beltran <sim6@probeta.net>
2018-03-22 11:07:13 +0100

    Fix upload_review path

--- project.py
+++ project.py
@@ -216,7 +216,7 @@ def upload_review(work, path, branch, review_name, review=None, new=False):
         sys.exit(1)
     work = tasks[0]
 
-    module = path.split('/')[-1]
+    module = os.path.realpath(path).split('/')[-1]
     components = Component.find([('name', '=', module)])
     if not components:
         component = Component(name=module)
@@ -229,7 +229,7 @@ def upload_review(work, path, branch, review_name, review=None, new=False):
     url_list = url.split('/')
     owner, repo_name = (url_list[-2], url_list[-1])
 
-    review = reviewboard.create(module, work.rec_name,
+    review = reviewboard.create(path, work.rec_name,
             (work.problem or '') + '\n' + (work.solution or ''), work.code)
 
     review_id = review
