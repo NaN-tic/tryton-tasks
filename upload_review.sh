d995438df770848550342a12ddbd3260b2737fdc
Raimon Esteve <raimon@nan-tic.com>
2019-09-18 13:09:22 +0200

    Module parameter when upload a review

--- upload_review.sh
+++ upload_review.sh
@@ -4,4 +4,4 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     exit 0
 fi
 
-invoke project.upload_review --work "#$1" --path $2 --branch $3
+invoke project.upload_review --work "#$1" --path $2 --branch $3 --module $4
