ee9107848f8f5886474233663506276af2514716
Raimon Esteve <raimonesteve@gmail.com>
2022-08-31 15:36:44 +0200

    requirements tasks

--- __init__.py
+++ __init__.py
@@ -7,9 +7,15 @@ from .tryton import TrytonCollection
 from .patches import QuiltCollection
 from .sao import SaoCollection
 
+try:
+    import trytond
+except ImportError:
+    trytond = None
+
 ns.add_collection(BootstrapCollection, 'bs')
 ns.add_collection(UtilsCollection, 'utils')
 ns.add_collection(ConfigCollection, 'config')
-ns.add_collection(TrytonCollection, 'tryton')
 ns.add_collection(QuiltCollection, 'quilt')
 ns.add_collection(SaoCollection, 'sao')
+if trytond:
+    ns.add_collection(TrytonCollection, 'tryton')
